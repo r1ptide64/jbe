d3d437ffbee3cd55f4b29302112b2dbbdc2e6a04
Josh Gordon <josh.h.gordon@gmail.com>
2016-11-29 23:01:45 -0600

    removing porchlight timer for the holidays

--- index.js
+++ index.js
@@ -1,15 +1,33 @@
-var express = require('express'),
-    router  = express.Router(),
-    path    = require('path');
+var express  = require('express'),
+    router   = express.Router(),
+    path     = require('path'),
+    execFile = require('child_process').execFile;
+
+var options = {
+    root: path.join(__dirname, 'public')
+};
 
 // GET home page.
 router.get('/', function (req, res, next) {
-    var options = {
-        root: path.join(__dirname, 'public')
-    };
     res.sendFile('/index.html', options, function (err) {
         if (err) {
             next(err);
+        }
+    });
+});
+
+// WOL
+router.get('/wol', function (req, res, next) {
+    execFile('/home/joeb/wol.sh', (err) => {
+        if (err) {
+            next(err);
+        }
+        else {
+            res.sendFile('/pug.gif', options, (err) => {
+                if (err) {
+                    next(err);
+                }
+            });
         }
     });
 });
