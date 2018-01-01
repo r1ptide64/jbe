0129a9c82dc9be99e1dd932e47567b2f0316037d
Joeb <Joeb@JOEB-DESK17>
2017-12-31 19:37:43 -0600

    testing api endpoint (that's 2 typos!!)

--- api.js
+++ api.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var router  = express.Router();
 
-router.get('/', function (req, resp, next) {
+router.get('/', function (req, res, next) {
     res.sendStatus(204);
 });
 
