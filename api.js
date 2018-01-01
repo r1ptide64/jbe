ad928eb50a856208919b41d37f1fdc36be7410c4
Joeb <Joeb@JOEB-DESK17>
2017-12-31 19:59:38 -0600

    add post to api endpoint

--- api.js
+++ api.js
@@ -1,8 +1,13 @@
 var express = require('express');
 var router  = express.Router();
+var debug   = require('debug')('jbe:api');
 
 router.get('/', function (req, res, next) {
     res.sendStatus(204);
 });
 
-module.export = router;
+router.post('/', function (req, res, next) {
+    debug(JSON.stringify(req.body, null, '\t'));
+});
+
+module.exports = router;
